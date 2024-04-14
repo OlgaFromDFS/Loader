e35a143547ff80b2b78912382b411f291ae22ad1
Olga <oluaribalka@yandex.ru>
2024-04-14 13:41:34 +0300

    a completely solved problem

--- src/components/General/General.tsx
+++ src/components/General/General.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { FC } from 'react';
 
 import { cnGeneral } from './General.classname';
@@ -7,16 +7,20 @@ import './General.css';
 
 import { Loader } from '../Loader/Loader';
 import { Image } from '../Image/Image';
-import { StupidLoader } from '../StupidLoader/StupidLoader';
 
 const General: FC = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChangeValue = (value: number) => {
+    setValue(value);
+  };
+
   return (
     <div className={cnGeneral()}>
-      <Loader />
-      <Image />
-      <StupidLoader />
+      <Loader onChange={handleChangeValue} value={value} />
+      {value === 100 && <Image />}
     </div>
-  );
-}
+  )
+};
 
 export { General };
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -8,7 +8,7 @@ import './Image.css';
 const Image: FC = () => {
   return (
     <img className={cnImage()} src='https://polinka.top/uploads/posts/2023-05/1684873907_polinka-top-p-ura-kartinka-radost-vkontakte-23.jpg'/>
-  );
-}
+  )
+};
 
 export { Image };
